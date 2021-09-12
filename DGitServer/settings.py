6d1398c0883ca59d31a5adab581d9434c1c42e9e
Dev <devaduthan.n@beinex.com>
2021-09-12 18:12:35 +0530

    allowed hosts

--- DGitServer/settings.py
+++ DGitServer/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-8momumbqj@@l%=ac_51skouh*5ykrw5uvuy$286ieu##9-_@#7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
